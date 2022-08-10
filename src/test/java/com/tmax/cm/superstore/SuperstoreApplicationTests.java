ab39d795dab84a16118362260d0524a9730b48f9
wootae_lim <wootae_lim@tmax.co.kr>
2022-08-10 13:53:03 +0900

    feat: superstore 프로젝트 추가

---
+++ src/test/java/com/tmax/cm/superstore/SuperstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tmax.cm.superstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
