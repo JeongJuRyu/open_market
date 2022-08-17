cd64001d00642600611db3365ec29f9935a5fd66
ryujeongju <totw2018@naver.com>
2022-08-17 15:33:01 +0900

    Add Spring security

--- src/main/java/com/tmax/cm/superstore/user/entities/User.java
+++ src/main/java/com/tmax/cm/superstore/user/entities/User.java
@@ -1,20 +1,68 @@
 package com.tmax.cm.superstore.user.entities;
 
+import java.util.Collection;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
+@Builder(builderMethodName = "UserBuilder")
 @NoArgsConstructor
-public class User {
+@AllArgsConstructor
+@Table(name="users")
+public class User implements UserDetails {
 	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "USER_ID")
 	private Long id;
 
+	@Column(unique = true, nullable = false)
+	private String email;
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return null;
+	}
+
+	@Override
+	public String getPassword() {
+		return null;
+	}
+
+	@Override
+	public String getUsername() {
+		return null;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return false;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return false;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return false;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return false;
+	}
 }
