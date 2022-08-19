b1e4a3b86f9bb738132cbd3b138a8e32fc3f592d
ryujeongju <totw2018@naver.com>
2022-08-19 16:30:16 +0900

    Remove Login Filter

---
+++ src/main/java/com/tmax/cm/superstore/common/config/security/JwtLoginFilter.java
@@ -0,0 +1,23 @@
+/*package com.tmax.cm.superstore.common.config.security;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JwtLoginFilter extends UsernamePasswordAuthenticationFilter {
+	public JwtLoginFilter(AuthenticationManager authenticationManager) {
+		super.setAuthenticationManager(authenticationManager);
+	}
+
+	@Override
+	public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws
+		AuthenticationException {
+		return super.attemptAuthentication(request, response);
+	}
+}*/
