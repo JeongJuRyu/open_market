e9d738236c0043677960fa1a669bf7341b3985f5
sangjoon_han <sazfirst@gmail.com>
2022-08-19 17:46:22 +0900

    feat: CORS allowed origin에 swagge 주소 추가

---
+++ src/main/java/com/tmax/cm/superstore/config/WebConfig.java
@@ -0,0 +1,15 @@
+package com.tmax.cm.superstore.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://192.168.159.41:28080/");
+    }
+}
