19d266ff3a784e95a1c21936b361768989733b62
jeongju_ryu <jeongju_ryu@tmax.co.kr>
2022-08-24 07:39:01 +0900

    feat: review mapper 추가

--- src/main/java/com/tmax/cm/superstore/mypage/mapper/ReviewMapper.java
+++ src/main/java/com/tmax/cm/superstore/mypage/mapper/ReviewMapper.java
@@ -1,7 +1,27 @@
 package com.tmax.cm.superstore.mypage.mapper;
 
+import com.tmax.cm.superstore.config.CommonMapperConfig;
+import com.tmax.cm.superstore.mypage.dto.GetAllReviewRequestDto;
+import com.tmax.cm.superstore.mypage.dto.GetAllReviewResponseDto;
+import com.tmax.cm.superstore.mypage.entity.Review;
+import com.tmax.cm.superstore.mypage.entity.ReviewImage;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
-@Mapper(componentModel = "spring")
+import java.util.List;
+
+@Mapper(config = CommonMapperConfig.class)
 public interface ReviewMapper {
+    @Mapping(target = "reviews", source = "reviews")
+    GetAllReviewResponseDto toResponse(List<Review> reviews);
+
+    @Mapping(target = "id", source = "review.id")
+    @Mapping(target = "title", source = "review.title")
+    @Mapping(target = "content", source = "review.content")
+    @Mapping(target = "createdAt", source = "review.createdAt")
+    @Mapping(target = "reviewImages", source = "review.reviewImages")
+    GetAllReviewResponseDto.ReviewDto toReviewDto(Review review);
+
+    @Mapping(target = "url", source = "reviewImage.url")
+    GetAllReviewResponseDto.ReviewDto.ReviewImage toReviewImageDto(ReviewImage reviewImage);
 }
