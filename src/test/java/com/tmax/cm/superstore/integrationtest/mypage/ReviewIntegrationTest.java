2363dabb81a11c38d2d1b9da4c92dbd6bd58c7d3
jeongju_ryu <jeongju_ryu@tmax.co.kr>
2022-08-25 13:50:20 +0900

    feat: 통합테스트 template 구성

---
+++ src/test/java/com/tmax/cm/superstore/integrationtest/mypage/ReviewIntegrationTest.java
@@ -0,0 +1,9 @@
+package com.tmax.cm.superstore.integrationtest.mypage;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+@Transactional
+public class ReviewIntegrationTest {
+}
