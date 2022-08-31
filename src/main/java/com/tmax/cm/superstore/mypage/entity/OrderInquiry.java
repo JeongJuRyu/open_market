815ea0fba6a34af33d4798b5e7a1fa75b94d047c
jeongju_ryu <jeongju_ryu@tmax.co.kr>
2022-08-31 10:48:22 +0900

    feature: 주문 엔티티 변경

--- src/main/java/com/tmax/cm/superstore/mypage/entity/OrderInquiry.java
+++ src/main/java/com/tmax/cm/superstore/mypage/entity/OrderInquiry.java
@@ -7,6 +7,7 @@ import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
 import org.hibernate.annotations.GenericGenerator;
@@ -38,6 +39,7 @@ public class OrderInquiry {
 	private String content;
 
 	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "ORDER_ITEM_ID")
 	private OrderItem orderItem;
 
 	public void updateOrderInquiry(UpdateOrderInquiryRequestDto dto){
--- src/main/java/com/tmax/cm/superstore/order/entity/OrderItem.java
+++ src/main/java/com/tmax/cm/superstore/order/entity/OrderItem.java
@@ -1,5 +1,7 @@
 package com.tmax.cm.superstore.order.entity;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 import javax.persistence.Column;
@@ -7,11 +9,14 @@ import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
 import org.hibernate.annotations.GenericGenerator;
 
 import com.tmax.cm.superstore.item.entity.Item;
+import com.tmax.cm.superstore.mypage.entity.OrderInquiry;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -26,6 +31,7 @@ import lombok.NoArgsConstructor;
 public class OrderItem {
 	@Id @GeneratedValue(generator = "UUID" )
 	@GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+	@Column(name = "ORDER_ITEM_ID", columnDefinition = "BINARY(16)")
 	private UUID id;
 
 	@Column(nullable = false)
@@ -33,12 +39,18 @@ public class OrderItem {
 
 	//추후 옵션 테이블 추가 하여 세분화 예상
 	@Column(nullable = false)
-	private String option;
+	private String options;
 
 	@Column(nullable = false)
 	private String imageUrl;
 
+	@OneToMany(mappedBy = "orderItem")
+	@Builder.Default
+	private List<OrderInquiry> orderInquiries = new ArrayList<>();
+
 	@OneToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "id")
 	private Item item;
+
 
 }
