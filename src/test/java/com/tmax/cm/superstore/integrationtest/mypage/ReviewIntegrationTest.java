1e456a2d78a4df434448f5a3038478eeaf3e15ea
jeongju_ryu <jeongju_ryu@tmax.co.kr>
2022-09-03 14:48:08 +0900

    refactor: Review, User 테스트 코드 추가

--- src/test/java/com/tmax/cm/superstore/integrationtest/mypage/ReviewIntegrationTest.java
+++ src/test/java/com/tmax/cm/superstore/integrationtest/mypage/ReviewIntegrationTest.java
@@ -1,12 +1,38 @@
-package com.tmax.cm.superstore.integrationtest.mypage;
-
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.restdocs.RestDocumentationExtension;
-import org.springframework.transaction.annotation.Transactional;
-
-@SpringBootTest
-@Transactional
-@ExtendWith(RestDocumentationExtension.class)
-public class ReviewIntegrationTest {
-}
+// package com.tmax.cm.superstore.integrationtest.mypage;
+//
+// import org.junit.jupiter.api.BeforeEach;
+// import org.junit.jupiter.api.Test;
+// import org.junit.jupiter.api.extension.ExtendWith;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.boot.test.context.SpringBootTest;
+// import org.springframework.restdocs.RestDocumentationContextProvider;
+// import org.springframework.restdocs.RestDocumentationExtension;
+// import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation;
+// import org.springframework.security.web.access.channel.ChannelProcessingFilter;
+// import org.springframework.test.context.ActiveProfiles;
+// import org.springframework.test.web.servlet.MockMvc;
+// import org.springframework.test.web.servlet.MockMvcBuilder;
+// import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+// import org.springframework.transaction.annotation.Transactional;
+// import org.springframework.web.context.WebApplicationContext;
+// import org.springframework.web.filter.CharacterEncodingFilter;
+//
+// @SpringBootTest
+// @Transactional
+// @ExtendWith(RestDocumentationExtension.class)
+// @ActiveProfiles("develop-integration-test")
+// public class ReviewIntegrationTest {
+// 	@Autowired
+// 	private WebApplicationContext context;
+//
+// 	private MockMvc mvc;
+//
+// 	@BeforeEach
+// 	public void setUp(RestDocumentationContextProvider restDocumentation){
+// 		this.mvc = MockMvcBuilders.webAppContextSetup(context)
+// 			.addFilter(new CharacterEncodingFilter("UTF-8", true))
+// 			.apply(MockMvcRestDocumentation.documentationConfiguration(restDocumentation))
+// 			.build();
+// 	}
+//
+// }
