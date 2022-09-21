dc9e9ae2e787e156f0e93a2d0a6496fbb72bfb68
jeongju_ryu <jeongju_ryu@tmax.co.kr>
2022-09-21 20:10:51 +0900

    feature: 리뷰 답변 컨트롤러, 서비스 분리

--- src/main/java/com/tmax/cm/superstore/mypage/controller/ReviewController.java
+++ src/main/java/com/tmax/cm/superstore/mypage/controller/ReviewController.java
@@ -2,7 +2,7 @@ package com.tmax.cm.superstore.mypage.controller;
 
 import java.util.UUID;
 
-import com.tmax.cm.superstore.mypage.dto.PostReviewReplyRequestDto;
+import com.tmax.cm.superstore.mypage.dto.GetAllReviewForSellerResponseDto;
 import com.tmax.cm.superstore.mypage.dto.PostReviewRequestDto;
 import com.tmax.cm.superstore.mypage.dto.GetAllReviewResponseDto;
 import com.tmax.cm.superstore.mypage.dto.GetReviewResponseDto;
@@ -46,13 +46,13 @@ public class ReviewController {
 		@PathVariable UUID reviewId){
 		return ResponseEntity.ok().body(reviewService.getReview(reviewId));
 	}
-	@PostMapping("/reply")
-	public ResponseEntity<UUID> createReviewReply(
-		@RequestBody PostReviewReplyRequestDto postReviewReplyRequestDto) {
-		return ResponseEntity.ok().body(reviewService.postReviewReply(postReviewReplyRequestDto));
-	}
 	@GetMapping("/getAll")
 	public ResponseEntity<GetAllReviewResponseDto> getAllReviewByItemId(@RequestParam UUID itemId){
 		return ResponseEntity.ok().body(reviewService.getAllReview(itemId));
+	}
+
+	@GetMapping("/seller")
+	public ResponseEntity<GetAllReviewForSellerResponseDto> getAllReviewForSeller(@RequestParam Long filterDay){
+		return ResponseEntity.ok().body(reviewService.getAllReviewForSeller(filterDay));
 	}
 }
