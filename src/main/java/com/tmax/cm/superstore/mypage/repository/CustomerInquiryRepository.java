cb280a3766541de56fe3a2f0b5a8aaa9d9b13b99
jeongju_ryu <jeongju_ryu@tmax.co.kr>
2022-09-22 20:17:16 +0900

    feature: 고객센터 문의 답변 API 추가

--- src/main/java/com/tmax/cm/superstore/mypage/repository/CustomerInquiryRepository.java
+++ src/main/java/com/tmax/cm/superstore/mypage/repository/CustomerInquiryRepository.java
@@ -1,6 +1,7 @@
 package com.tmax.cm.superstore.mypage.repository;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 import com.tmax.cm.superstore.mypage.entity.CustomerInquiry;
@@ -8,8 +9,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 public interface CustomerInquiryRepository extends JpaRepository<CustomerInquiry, UUID> {
-	@Query(value = "select ci from CustomerInquiry ci join fetch ci.customerInquiryAnswers "
+	@Query(value = "select ci from CustomerInquiry ci join fetch ci.customerInquiryReplies "
 		+ "join fetch ci.customerInquiryImages "
-		+ "where ci.user.id =: userId", nativeQuery = true)
+		+ "where ci.user.id = :userId")
 	List<CustomerInquiry> findAllByUserId(UUID userId);
+
+	@Query("select ci from CustomerInquiry ci join fetch ci.customerInquiryReplies"
+		+ " where ci.id = :customerInquiryId")
+	Optional<CustomerInquiry> findByIdWithReply(UUID customerInquiryId);
 }
