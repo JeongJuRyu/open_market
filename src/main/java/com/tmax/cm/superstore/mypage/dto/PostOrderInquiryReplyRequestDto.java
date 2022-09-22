e828414fb3af3e5e9c69f0b1d5c84c0bdfc234b5
jeongju_ryu <jeongju_ryu@tmax.co.kr>
2022-09-22 18:05:00 +0900

    feature: 주문 상품 답변 API 추가

---
+++ src/main/java/com/tmax/cm/superstore/mypage/dto/PostOrderInquiryReplyRequestDto.java
@@ -0,0 +1,13 @@
+package com.tmax.cm.superstore.mypage.dto;
+
+import java.util.UUID;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class PostOrderInquiryReplyRequestDto {
+	private UUID orderInquiryId;
+	private String content;
+}
