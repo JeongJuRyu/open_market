9efb1b5b754726469fec0349594d71ba72b2b121
jeongju_ryu <jeongju_ryu@tmax.co.kr>
2022-10-04 09:58:17 +0900

    feature: 배송지 추가, 수정, 삭제 API 추가

---
+++ src/main/java/com/tmax/cm/superstore/user/dto/UpdateDeliveryInfoRequestDto.java
@@ -0,0 +1,30 @@
+package com.tmax.cm.superstore.user.dto;
+
+import java.util.UUID;
+
+import javax.validation.constraints.NotNull;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class UpdateDeliveryInfoRequestDto {
+	@NotNull
+	private UUID shippingAddressId;
+
+	@NotNull
+	private String email;
+
+	@NotNull
+	private String recipient;
+
+	@NotNull
+	private boolean isDefaultAddress;
+
+	@NotNull
+	private String mobile;
+
+	@NotNull
+	private String address;
+}
