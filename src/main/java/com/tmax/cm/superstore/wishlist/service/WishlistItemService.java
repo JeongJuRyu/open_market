21b661b3ddf1e78a66229264aa88a238be1ea010
hyunjun-cho <gusrkfl0609@ajou.ac.kr>
2022-10-07 13:36:08 +0900

    refactor: 찜한 상품 예외처리

--- src/main/java/com/tmax/cm/superstore/wishlist/service/WishlistItemService.java
+++ src/main/java/com/tmax/cm/superstore/wishlist/service/WishlistItemService.java
@@ -7,12 +7,15 @@ import com.tmax.cm.superstore.item.repository.ItemRepository;
 import com.tmax.cm.superstore.wishlist.dto.DeleteWishlistItemsDto;
 import com.tmax.cm.superstore.wishlist.dto.PostCreateWishlistItemDto;
 import com.tmax.cm.superstore.wishlist.entity.WishlistItem;
+import com.tmax.cm.superstore.wishlist.error.exception.WishlistItemAlreadyExistsException;
 import com.tmax.cm.superstore.wishlist.repository.WishlistItemRepository;
+import com.tmax.cm.superstore.wishlist.service.dto.DeleteWishlistItemByItemIdDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
 import java.util.List;
+import java.util.UUID;
 
 @RequiredArgsConstructor
 @Service
@@ -41,6 +44,9 @@ public class WishlistItemService {
 
     @Transactional
     public WishlistItem create(PostCreateWishlistItemDto.Request itemDto) {
+        if(this.checkItem(itemDto.getItemId()))
+            throw new WishlistItemAlreadyExistsException();
+
         Item item =  this.itemRepository.findById(itemDto.getItemId())
                 .orElseThrow(ItemNotFoundException::new);
 
@@ -50,5 +56,18 @@ public class WishlistItemService {
 
         this.wishlistItemRepository.save(wishlistItem);
         return wishlistItem;
+    }
+
+    @Transactional
+    public Boolean checkItem(UUID itemId) {
+        return this.wishlistItemRepository.findAll().stream().anyMatch(wishlistItem ->
+                wishlistItem.getItem().getId().equals(itemId));
+    }
+
+    @Transactional
+    public void deleteByItemId(DeleteWishlistItemByItemIdDto requestDto) {
+        WishlistItem wishlistItem = this.wishlistItemRepository.findByItemId(requestDto.getItemId())
+                .orElseThrow(() -> EntityNotFoundException.of(WishlistItem.class));
+        wishlistItem.setIsDeleted(true);
     }
 }
