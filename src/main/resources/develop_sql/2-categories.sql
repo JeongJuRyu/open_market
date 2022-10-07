a0199a636c865af1c9fed3dbcdde25e0d5ddc503
sangjoon_han <sazfirst@gmail.com>
2022-10-07 15:56:58 +0900

    feat: 초기 로드 sql 여러 파일로 분리

---
+++ src/main/resources/develop_sql/2-categories.sql
@@ -0,0 +1,125 @@
+insert into categories values (1,'여성패션', 0);
+insert into categories values (2,'남성패션', 0);
+insert into categories values (3,'화장품/미용', 0);
+insert into categories values (4,'가구/인테리어', 0);
+insert into categories values (5,'식품', 0);
+insert into categories values (6,'출산/유아동', 0);
+
+insert into categories values (7,'여성의류', 1);
+insert into categories values (8,'여성신발', 1);
+insert into categories values (9,'여성가방/지갑', 1);
+insert into categories values (10,'주얼리', 1);
+insert into categories values (11,'언더웨어/잠옷', 1);
+insert into categories values (12,'시계', 1);
+insert into categories values (13,'패션잡화/소품', 1);
+
+insert into categories values (51,'원피스', 7);
+insert into categories values (52,'상의', 7);
+insert into categories values (53,'하의', 7);
+insert into categories values (54,'아우터', 7);
+insert into categories values (55,'수트/셋업', 7);
+insert into categories values (56,'운동복', 7);
+insert into categories values (57,'수영복/비치웨어', 7);
+insert into categories values (58,'테마의류', 7);
+insert into categories values (59,'임산부의류', 7);
+
+insert into categories values (60,'미니', 51);
+insert into categories values (61,'미디움', 51);
+insert into categories values (62,'롱', 51);
+
+insert into categories values (63,'크롭티', 52);
+insert into categories values (64,'긴팔티셔츠', 52);
+insert into categories values (65,'반팔티셔츠', 52);
+insert into categories values (66,'후드티', 52);
+insert into categories values (67,'맨투맨/스웨트셔츠', 52);
+insert into categories values (68,'블라우스/셔츠', 52);
+insert into categories values (69,'니트/스웨터', 52);
+
+insert into categories values (70,'스커트', 53);
+insert into categories values (71,'청바지', 53);
+insert into categories values (72,'팬츠', 53);
+insert into categories values (73,'오버롤', 53);
+insert into categories values (74,'레깅스', 53);
+
+insert into categories values (75,'트위드재킷', 54);
+insert into categories values (76,'카디건', 54);
+insert into categories values (77,'조끼', 54);
+insert into categories values (78,'레인코트', 54);
+insert into categories values (79,'바람막이', 54);
+insert into categories values (80,'재킷', 54);
+insert into categories values (81,'코트', 54);
+insert into categories values (82,'점퍼', 54);
+insert into categories values (83,'핸드메이드코트', 54);
+
+insert into categories values (84,'수트', 55);
+insert into categories values (85,'점프수트', 55);
+insert into categories values (86,'트위드셋업', 55);
+insert into categories values (87,'니트셋업', 55);
+insert into categories values (88,'쇼츠셋업', 55);
+insert into categories values (89,'트레이닝셋업', 55);
+insert into categories values (90,'패턴셋업', 55);
+
+insert into categories values (91,'요가/필라테스', 56);
+insert into categories values (92,'트레이닝상의', 56);
+insert into categories values (93,'트레이닝하의', 56);
+insert into categories values (94,'트레이닝세트', 56);
+
+insert into categories values (95,'비키니', 57);
+insert into categories values (96,'원피스', 57);
+insert into categories values (97,'래쉬가드', 57);
+insert into categories values (98,'워터레깅스', 57);
+
+insert into categories values (99,'한복', 58);
+insert into categories values (100,'파티복', 58);
+insert into categories values (101,'코스튬', 58);
+insert into categories values (102,'유니폼/단체복', 58);
+
+insert into categories values (103,'언더웨어', 59);
+insert into categories values (104,'수유복', 59);
+insert into categories values (105,'원피스', 59);
+insert into categories values (106,'바지', 59);
+insert into categories values (107,'청바지', 59);
+insert into categories values (108,'트레이닝복/요가복', 59);
+insert into categories values (109,'스커트', 59);
+insert into categories values (110,'스타킹', 59);
+insert into categories values (111,'레깅스', 59);
+
+insert into categories values (14,'남성의류', 2);
+insert into categories values (15,'남성신발', 2);
+insert into categories values (16,'남성가방/지갑', 2);
+insert into categories values (17,'주얼리', 2);
+insert into categories values (18,'언더웨어/잠옷', 2);
+insert into categories values (19,'시계', 2);
+insert into categories values (20,'패션잡화/소품', 2);
+
+insert into categories values (21,'스킨케어', 3);
+insert into categories values (22,'헤어케어', 3);
+insert into categories values (23,'바디케어', 3);
+insert into categories values (24,'향수', 3);
+insert into categories values (25,'클렌징', 3);
+insert into categories values (26,'뷰티소품', 3);
+insert into categories values (27,'베이스메이크업', 3);
+insert into categories values (28,'섹조메이크업', 3);
+insert into categories values (29,'마스크/팩', 3);
+insert into categories values (30,'네일케어', 3);
+insert into categories values (31,'선케어', 3);
+insert into categories values (32,'남성화장품', 3);
+insert into categories values (33,'비건', 3);
+
+insert into categories values (34,'소파', 4);
+insert into categories values (35,'침대/매트리스', 4);
+insert into categories values (36,'드레스룸/행거', 4);
+insert into categories values (37,'거실장/테이블', 4);
+insert into categories values (38,'책상/책장', 4);
+insert into categories values (39,'의자', 4);
+insert into categories values (40,'식탁/주방가구', 4);
+insert into categories values (41,'선반/수납가구', 4);
+insert into categories values (42,'아동/주니어', 4);
+insert into categories values (43,'조명/스텐드', 4);
+insert into categories values (44,'셀프인테리어', 4);
+insert into categories values (45,'시공/리모델링', 4);
+insert into categories values (46,'홈데코/ 수예', 4);
+insert into categories values (47,'침구', 4);
+insert into categories values (48,'카페트/러그', 4);
+insert into categories values (49,'커튼/블라인드', 4);
+insert into categories values (50,'야외가구', 4);
---
+++ src/main/resources/integration-test-h2_sql/99-last.sql
@@ -0,0 +1,12 @@
+--- 유저 등록
+INSERT INTO users(user_id, email, password, phone_num, address, user_name, name) VALUES (UUID'672ffb8c-f952-49ec-b65b-4fe3a9c37b28', 'totw2018@naver.com', '1234', '010-4523-6994', '사랑시 행복구 고백동', '류정주', '류정주');
+
+--- 찜 그룹 등록
+INSERT INTO wishlist_group(id, name, position, is_deleted) VALUES (1, '여행용', 1, false);
+INSERT INTO wishlist_group(id, name, position, is_deleted) VALUES (2, '나들이용', 2, false);
+
+--- 찜 상품 등록
+INSERT INTO wishlist_item(id, item_id, wishlist_group_id, is_deleted) VALUES (1, UUID'169f84f8-8862-477c-ad27-0b79871deb27', 1, false);
+INSERT INTO wishlist_item(id, item_id, wishlist_group_id, is_deleted) VALUES (2, UUID'169f84f8-8862-477c-ad27-0b79871deb28', 1, false);
+INSERT INTO wishlist_item(id, item_id, wishlist_group_id, is_deleted) VALUES (3, UUID'169f84f8-8862-477c-ad27-0b79871deb29', 2, false);
+INSERT INTO wishlist_item(id, item_id, wishlist_group_id, is_deleted) VALUES (4, UUID'169f84f8-8862-477c-ad27-0b79871deb30', null, false);
