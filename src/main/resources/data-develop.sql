b9923450d9418d9cc26a33b78e3e4855e4a911ad
jeongju_ryu <jeongju_ryu@tmax.co.kr>
2022-10-11 15:23:43 +0900

    feature: 더미데이터 sql 수정

--- src/main/resources/data-develop.sql
+++ src/main/resources/data-develop.sql
@@ -224,7 +224,7 @@ INSERT INTO cart_option(id, count, cart_option_group_id, option_id) VALUES (UUID
 -- 카테고리 상품
 
 -- 유저 등록
-INSERT INTO USERS(user_id, email, password, phone_num, address, user_name, nick_name) VALUES (UUID_TO_BIN('672ffb8c-f952-49ec-b65b-4fe3a9c37b28'), 'totw2018@naver.com', '1234', '010-4523-6994', '사랑시 행복구 고백동', '류정주', '쩡류');
+INSERT INTO USERS(user_id, email, password, phone_num, address, name) VALUES (UUID_TO_BIN('672ffb8c-f952-49ec-b65b-4fe3a9c37b28'), 'totw2018@naver.com', '1234', '010-4523-6994', '사랑시 행복구 고백동', '류정주');
 
 -- 찜 그룹 등록
 INSERT INTO wishlist_group(id, name, position, is_deleted) VALUES (1, '여행용', 1, false);
@@ -236,19 +236,21 @@ INSERT INTO wishlist_item(id, item_id, wishlist_group_id, is_deleted) VALUES (2,
 INSERT INTO wishlist_item(id, item_id, wishlist_group_id, is_deleted) VALUES (3, UUID_TO_BIN('169f84f8-8862-477c-ad27-0b79871deb29'), 2, false);
 INSERT INTO wishlist_item(id, item_id, wishlist_group_id, is_deleted) VALUES (4, UUID_TO_BIN('169f84f8-8862-477c-ad27-0b79871deb30'), null, false);
 
--- 리뷰 등록
-INSERT INTO REVIEW(review_id, content, is_useful, star_rating, title, item_id, user_id) VALUES (UUID_TO_BIN('ce093467-1e60-45c9-b073-9b684b9dbc06'), '맛있어요', 23, 4.0, '추천',UUID_TO_BIN('1523bc68-e8f7-4140-b7dd-cbfe622e068a'), UUID_TO_BIN('672ffb8c-f952-49ec-b65b-4fe3a9c37b28'));
-INSERT INTO REVIEW(review_id, content, is_useful, star_rating, title, item_id, user_id) VALUES (UUID_TO_BIN('ea9e4424-3641-4bcc-b75a-8ded708c5b4f'), '최곤데요?', 10, 5.0, '강추',UUID_TO_BIN('1523bc68-e8f7-4140-b7dd-cbfe622e068a'), UUID_TO_BIN('672ffb8c-f952-49ec-b65b-4fe3a9c37b28'));
-INSERT INTO REVIEW(review_id, content, is_useful, star_rating, title, item_id, user_id) VALUES (UUID_TO_BIN('885d458c-997a-4e67-b410-39fae05606f4'), '어른들 간식, 아이들 술안주로 딱입니다.', 100, 3.0, '그냥 그래요', UUID_TO_BIN('1523bc68-e8f7-4140-b7dd-cbfe622e068a'), UUID_TO_BIN('672ffb8c-f952-49ec-b65b-4fe3a9c37b28'));
+-- 배송료 등록
 
--- 리뷰 이미지 등록
-INSERT INTO review_image(review_image_id, url, reivew_id) values (UUID_TO_BIN('a303a658-fca9-4fc4-8603-5d04bdd52126'), 'https://naver.com', UUID_TO_BIN('ce093467-1e60-45c9-b073-9b684b9dbc06'));
-INSERT INTO review_image(review_image_id, url, reivew_id) values (UUID_TO_BIN('7de948f4-80ba-43e1-9da6-891b2713a581'), 'https://tmax.co.kr', UUID_TO_BIN('ce093467-1e60-45c9-b073-9b684b9dbc06'));
+-- 주문 상품 등록
+# insert into order_item(id, amount, count, image_url, name, price) values (UUID_TO_BIN('bf28593f-71ad-4418-b82d-fceea62e9167'), 3, 1, 'http://naver.com', '불고기', 12000);
+# insert into order_item(id, amount, count, image_url, name, price) values (UUID_TO_BIN('eb879118-4cfc-41b4-ab3f-451b23f1d74c'), 2, 4, 'http://naver.com', '꽃등심', 25000);
+# -- 리뷰 등록
+# INSERT INTO REVIEW(review_id, content, is_useful, star_rating, title, item_id, user_id, order_item_id) VALUES (UUID_TO_BIN('ce093467-1e60-45c9-b073-9b684b9dbc06'), '맛있어요',true, 23, 4.0, '추천',UUID_TO_BIN('1523bc68-e8f7-4140-b7dd-cbfe622e068a'), UUID_TO_BIN('672ffb8c-f952-49ec-b65b-4fe3a9c37b28'), );
+# INSERT INTO REVIEW(review_id, content, is_useful, star_rating, title, item_id, user_id, order_item_id) VALUES (UUID_TO_BIN('ea9e4424-3641-4bcc-b75a-8ded708c5b4f'), '최곤데요?', true, 10, 5.0, '강추',UUID_TO_BIN('1523bc68-e8f7-4140-b7dd-cbfe622e068a'), UUID_TO_BIN('672ffb8c-f952-49ec-b65b-4fe3a9c37b28'));
+# INSERT INTO REVIEW(review_id, content, is_useful, star_rating, title, item_id, user_id, order_item_id) VALUES (UUID_TO_BIN('885d458c-997a-4e67-b410-39fae05606f4'), '어른들 간식, 아이들 술안주로 딱입니다.', false, 100, 3.0, '그냥 그래요', UUID_TO_BIN('1523bc68-e8f7-4140-b7dd-cbfe622e068a'), UUID_TO_BIN('672ffb8c-f952-49ec-b65b-4fe3a9c37b28'));
+
 
 -- 리뷰 답변 등록
 
-INSERT INTO review_reply(review_reply_id, content, reivew_id) values (UUID_TO_BIN('6d269b51-590c-4ae4-95eb-e0b447b47c24'), '감사합니다.', UUID_TO_BIN('ce093467-1e60-45c9-b073-9b684b9dbc06'));
+# INSERT INTO review_reply(review_reply_id, content, reivew_id) values (UUID_TO_BIN('6d269b51-590c-4ae4-95eb-e0b447b47c24'), '감사합니다.', UUID_TO_BIN('ce093467-1e60-45c9-b073-9b684b9dbc06'));
 
 -- 유저 배송지 등록
-INSERT INTO delivery_address(id, address, is_basic_address, name, phone_num, user_id) values (UUID_TO_BIN('143e9261-738b-49a4-99e7-94559560cb35'), '사랑시 고백구 행복동', true, '류정주', '010-4523-6994', UUID_TO_BIN('672ffb8c-f952-49ec-b65b-4fe3a9c37b28'));
-INSERT INTO delivery_address(id, address, is_basic_address, name, phone_num, user_id) values (UUID_TO_BIN('c89ba92a-6417-4478-aa9c-6adc7b2d3698'), '서울시 성북구 솔샘로7길 19, 103호', true, '김덕배', '010-1111-6994', UUID_TO_BIN('672ffb8c-f952-49ec-b65b-4fe3a9c37b28'));
+INSERT INTO delivery_address(id, address, is_default_address, mobile, recipient, requests, user_id) values (UUID_TO_BIN('143e9261-738b-49a4-99e7-94559560cb35'), '사랑시 고백구 행복동', true, '010-4523-6994', '류정주', '오이 빼주세요', UUID_TO_BIN('672ffb8c-f952-49ec-b65b-4fe3a9c37b28'));
+INSERT INTO delivery_address(id, address, is_default_address, mobile, recipient, requests, user_id) values (UUID_TO_BIN('c89ba92a-6417-4478-aa9c-6adc7b2d3698'), '서울시 성북구 솔샘로7길 19, 103호', true, '010-1111-6994', '김덕배', '오이 제발 빼주세요', UUID_TO_BIN('672ffb8c-f952-49ec-b65b-4fe3a9c37b28'));
