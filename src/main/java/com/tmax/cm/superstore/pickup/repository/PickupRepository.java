6bdb914744ffbd6d66358f435743f9bca351f496
hyunjun-cho <gusrkfl0609@ajou.ac.kr>
2022-10-17 13:00:15 +0900

    bugfix: 픽업/방문수령 삭제

--- src/main/java/com/tmax/cm/superstore/pickup/repository/PickupRepository.java
+++
@@ -1,9 +0,0 @@
-package com.tmax.cm.superstore.pickup.repository;
-
-import com.tmax.cm.superstore.pickup.entity.PickUp;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface PickUpRepository extends JpaRepository<PickUp, UUID> {
-}
