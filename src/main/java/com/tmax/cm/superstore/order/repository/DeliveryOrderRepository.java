4745962942885baef33c637023c5205f1060d730
sangjoon_han <sazfirst@gmail.com>
2022-10-19 17:04:10 +0900

    feat: 주문 조회 API에 상태에 따른 필터링 추가

--- src/main/java/com/tmax/cm/superstore/order/repository/DeliveryOrderRepository.java
+++ src/main/java/com/tmax/cm/superstore/order/repository/DeliveryOrderRepository.java
@@ -5,10 +5,14 @@ import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.tmax.cm.superstore.code.ShippingType;
 import com.tmax.cm.superstore.order.entity.DeliveryOrder;
 import com.tmax.cm.superstore.seller.entity.Seller;
 
 public interface DeliveryOrderRepository extends JpaRepository<DeliveryOrder, UUID> {
 
     List<DeliveryOrder> findBySeller(Seller seller);
+
+    List<DeliveryOrder> findBySellerAndShippingOrderItems_ShippingOrderSelectedOptions_Shipping_ShippingType(
+            Seller seller, ShippingType shippingType);
 }
