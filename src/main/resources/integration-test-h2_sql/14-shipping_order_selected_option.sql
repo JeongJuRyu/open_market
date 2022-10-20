14fb6413e7cb5c81bf790f1975c1c1357af4766d
sangjoon_han <sazfirst@gmail.com>
2022-10-20 15:19:11 +0900

    feat: orderSelectedOption 엔티티에 Shop 연결 추가

--- src/main/resources/integration-test-h2_sql/14-shipping_order_selected_option.sql
+++ src/main/resources/integration-test-h2_sql/14-shipping_order_selected_option.sql
@@ -3,26 +3,30 @@ INSERT INTO shipping_order_selected_option (
         amount,
         count,
         shipping_id,
-        shipping_order_item_id
+        shipping_order_item_id,
+        seller_seller_id
     )
 VALUES(
-        UUID '6148b3fe-7fdd-4344-8938-4d938bd23799',
+        UUID'6148b3fe-7fdd-4344-8938-4d938bd23799',
         81000,
         1,
-        UUID '0eceec16-cdab-4885-a552-066e2df2e3ad',
-        2
+        UUID'0eceec16-cdab-4885-a552-066e2df2e3ad',
+        2,
+        UUID'8d41c42a-011b-4525-9864-b24481f985c5'
     ),
     (
-        UUID '95547dc4-6d88-4796-b964-ff71aa056b7c',
+        UUID'95547dc4-6d88-4796-b964-ff71aa056b7c',
         82000,
         1,
-        UUID '0eceec16-cdab-4885-a552-066e2df2e3ad',
-        2
+        UUID'0eceec16-cdab-4885-a552-066e2df2e3ad',
+        2,
+        UUID'8d41c42a-011b-4525-9864-b24481f985c5'
     ),
     (
-        UUID '9c937a70-12cf-4a83-b594-1293b3f994a8',
+        UUID'9c937a70-12cf-4a83-b594-1293b3f994a8',
         21500,
         1,
-        UUID '2a39a689-b011-441d-a719-0ce4754baa4c',
-        1
+        UUID'2a39a689-b011-441d-a719-0ce4754baa4c',
+        1,
+        UUID'8d41c42a-011b-4525-9864-b24481f985c5'
     );
