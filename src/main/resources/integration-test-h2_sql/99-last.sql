b712f729e3c4823f9d46e1e30af59a1235070b29
hyunjun-cho <gusrkfl0609@ajou.ac.kr>
2022-10-20 10:17:34 +0900

    refactor: 테스트 데이터 스키마 수정

--- src/main/resources/integration-test-h2_sql/99-last.sql
+++ src/main/resources/integration-test-h2_sql/99-last.sql
@@ -1,8 +1,8 @@
 --- 유저 등록
 INSERT INTO users(user_id, email, password, phone_num, address, name, account_non_expired, account_non_locked) VALUES (UUID'672ffb8c-f952-49ec-b65b-4fe3a9c37b28', 'totw2018@naver.com', '1234', '010-4523-6994', '사랑시 행복구 고백동', '류정주', true, true);
 --- 찜 그룹 등록
-INSERT INTO wishlist_group(id, name, position, is_deleted) VALUES (1, '여행용', 1, false);
-INSERT INTO wishlist_group(id, name, position, is_deleted) VALUES (2, '나들이용', 2, false);
+INSERT INTO wishlist_group(id, name, position, is_deleted, user_user_id) VALUES (1, '여행용', 1, false, UUID'672ffb8c-f952-49ec-b65b-4fe3a9c37b28');
+INSERT INTO wishlist_group(id, name, position, is_deleted, user_user_id) VALUES (2, '나들이용', 2, false, UUID'672ffb8c-f952-49ec-b65b-4fe3a9c37b28');
 
 --- 찜 상품 등록
 INSERT INTO wishlist_item(id, item_id, wishlist_group_id, is_deleted) VALUES (1, UUID'169f84f8-8862-477c-ad27-0b79871deb27', 1, false);
